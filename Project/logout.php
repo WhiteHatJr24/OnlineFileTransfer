ddf911f7833297ec6ab9445ade20f9b8534b9e25
Aditya Mohanta <118840538+WhiteHatJr24@users.noreply.github.com>
2024-01-09 12:50:54 +0530

    Update logout.php

--- Project/logout.php
+++ Project/logout.php
@@ -1,4 +1,6 @@
 <?php
+header("Access-Control-Allow-Origin: http://allowed-origin.com");
+header("Access-Control-Allow-Methods: POST");
 session_start();
 session_destroy();
 header("Location:index.php");
