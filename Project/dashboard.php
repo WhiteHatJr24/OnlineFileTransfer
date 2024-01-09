d7df73946247145e982032846260a14e31105f69
Aditya Mohanta <118840538+WhiteHatJr24@users.noreply.github.com>
2024-01-09 12:49:40 +0530

    Update dashboard.php

--- Project/dashboard.php
+++ Project/dashboard.php
@@ -1,6 +1,8 @@
 <?php
 session_start();
 include('db.php');
+header("Access-Control-Allow-Origin: http://allowed-origin.com");
+header("Access-Control-Allow-Methods: POST");
 
 if (!isset($_SESSION['user_id'])) {
     header("Location: login.html");
