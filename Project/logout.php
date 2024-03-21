ebab8bb0e2dbe375009ecd8698eb171595421d67
Aditya Mohanta <118840538+WhiteHatJr24@users.noreply.github.com>
2024-03-22 00:10:55 +0530

    Delete Project directory

--- Project/logout.php
+++
@@ -1,8 +0,0 @@
-<?php
-header("Access-Control-Allow-Origin: http://allowed-origin.com");
-header("Access-Control-Allow-Methods: POST");
-session_start();
-session_destroy();
-header("Location:index.php");
-exit();
-?>
